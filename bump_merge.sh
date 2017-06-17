c5465e1386bb7c097d5f4396118354b791e7c531
Douglas Bruey <douglas.bruey@synapse.com>
2017-06-17 00:12:32 -0700

    Adding the first working version of bump merge

---
+++ bump_merge.sh
@@ -0,0 +1,41 @@
+# I want to keep the server's version when there is a conflict
+# %O (the first parameter) contains the ancestor version
+# %A (the second parameter) contains my "our" version
+# %B (the third parameter) contains the server's "their" version
+# We want to put our version in a local directory and keep the server's version
+echo "Merging using bump_merge.sh"
+#ls -la
+#FILES=`find -not \( -path '*/local_version/*' -prune \) -not \( -path ./.git -prune \) -a -type f -print`
+
+# Get a list of the files in conflict
+FILES=`git diff --name-only origin/master`
+
+# Find the local version of this particular file and move it to a local version folder
+LOCALDIR=local_version
+for f in $FILES
+do
+    if [[ -f $f ]]
+    then
+        NAME=$(basename $f)
+        DIRNAME=$(dirname $f)
+        #echo $NAME
+        #echo $DIRNAME
+        #echo $DIRNAME/$LOCALDIR
+        if [ "$2" != "$NAME" ]
+        then
+            echo "Processing $f:"
+            if cmp --silent $2 $f 
+            then
+                #echo "files are identical"
+                #stat $f
+                echo "Saving $f to local_version"
+                mkdir -p $DIRNAME/$LOCALDIR
+                cp -f $2 $DIRNAME/$LOCALDIR/$NAME
+            fi
+        fi
+    fi
+done
+
+# Use the incoming file in the working directory
+cp -f $3 $2
+exit 0
---
+++ merge_config.bat
@@ -0,0 +1,2 @@
+git config --global merge.bump.name "Bump local copy to local_version folder, keep server copy"
+git config --global merge.bump.driver "bump_merge.sh %%O %%A %%B"
